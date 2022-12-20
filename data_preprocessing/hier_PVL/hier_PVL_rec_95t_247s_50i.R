60b386c0b245e56873e0b45b26d1f5eb3e34b2a8
sigridbom <sigridabn@hotmail.com>
2022-12-20 15:09:12 +0100

    serious scripts

--- data_preprocessing/hier_PVL/hier_PVL_rec_95t_247s_50i.R
+++ data_preprocessing/hier_PVL/hier_PVL_rec_95t_247s_50i.R
@@ -1,12 +1,17 @@
 install.packages("pacman")
-pacman::p_load(R2jags, parallel, ggpubr, extraDistr, truncnorm)
+pacman::p_load(R2jags, parallel, ggpubr, extraDistr, truncnorm, tidyverse)
 
 set.seed(1982)
+
+setwd("/work/exam/dm_exam/data_preprocessing/hier_PVL")
+#getwd()
 
 # defining a function for calculating the maximum of the posterior density (not exactly the same as the mode)
 MPD <- function(x) {
   density(x)$x[which(density(x)$y==max(density(x)$y))]
 }
+
+
 
 #------ create task environment -------------------
 # NB! mod(ntrials, nstruct) (aka. ntrials %% nstruct) must be 0
@@ -38,38 +43,47 @@ D_R <- rep(good_r, nstruct)
 D_L <- c(rep(good_infreq_l, nstruct*infreq),rep(0,nstruct*(1-infreq)))
 
 # create the pseudorandomized full payoff structure
-A <- array(NA,ntrials) # setting up and empty array to be filled
-B <- array(NA,ntrials)
-C <- array(NA,ntrials)
-D <- array(NA,ntrials)
-for (i in 1:(ntrials/nstruct)) {
-  A[(1+(i-1)*nstruct):(i*nstruct)] <- (A_R + sample(A_L)) # randomly shuffling the loss-array for every ten trials (and adding those losses to the winnings)
-  B[(1+(i-1)*nstruct):(i*nstruct)] <- (B_R + sample(B_L))
-  C[(1+(i-1)*nstruct):(i*nstruct)] <- (C_R + sample(C_L))
-  D[(1+(i-1)*nstruct):(i*nstruct)] <- (D_R + sample(D_L))
-}
+# A <- array(NA,ntrials) # setting up and empty array to be filled
+# B <- array(NA,ntrials)
+# C <- array(NA,ntrials)
+# D <- array(NA,ntrials)
+# for (i in 1:(ntrials/nstruct)) {
+#   A[(1+(i-1)*nstruct):(i*nstruct)] <- (A_R + sample(A_L)) # randomly shuffling the loss-array for every ten trials (and adding those losses to the winnings)
+#   B[(1+(i-1)*nstruct):(i*nstruct)] <- (B_R + sample(B_L))
+#   C[(1+(i-1)*nstruct):(i*nstruct)] <- (C_R + sample(C_L))
+#   D[(1+(i-1)*nstruct):(i*nstruct)] <- (D_R + sample(D_L))
+# }
 
 # "less generic" code
-# A <- c()
-# B <- c()
-# C <- c()
-# D <- c()
-# for (i in 1:10) {
-#   A <- (append(A,A_R + sample(A_L)))
-#   B <- (append(B,B_R + sample(B_L)))
-#   C <- (append(C,C_R + sample(C_L)))
-#   D <- (append(D,D_R + sample(D_L)))
-# }
+A <- c()
+B <- c()
+C <- c()
+D <- c()
+for (i in 1:10) {
+  A <- (append(A,A_R + sample(A_L)))
+  B <- (append(B,B_R + sample(B_L)))
+  C <- (append(C,C_R + sample(C_L)))
+  D <- (append(D,D_R + sample(D_L)))
+}
 
 payoff <- cbind(A,B,C,D)/100 # combining all four decks as columns with each 100 trials - dividing our payoffs by 100 to make the numbers a bit easier to work with
 
 # let's look at the payoff
 colSums(payoff) # the two bad decks should sum to -25 (i.e. -2500), and the two good ones to 25 (i.e. 2500)
 
+#################
+# cut the last 5 rows of payoff 
+payoff <- as_tibble(payoff)
+payoff <- payoff %>% slice(1:95)
+payoff <- data.matrix(payoff)
+
+#############
+
 ###--------------Run full parameter recovery -------------
-niterations <- 100 # fewer because it takes too long
-nsubs <- 48 # mimicking the data structure from Ahn et al.
-ntrials_all <- rep(100, 48) # all 48 subs have 100 trials each
+niterations <- 50 # fewer because it takes too long
+nsubs <- 247 # mimicking the data structure from Ahn et al.
+ntrials_all <- rep(95, 247) # all 48 subs have 100 trials each
+ntrials <- 95
 
 # mu
 true_mu_w <- array(NA,c(niterations))
@@ -108,9 +122,9 @@ for (i in 1:niterations) {
   sigma_theta <- runif(1,0,0.2)
   sigma_a <- runif(1,0,0.1)
   
-  source('216377/Module4/hier_PVL_sim.R')
+  source('hier_PVL_sim.R')
   PVL_sims <- hier_PVL_sim(payoff,nsubs,ntrials,mu_w,mu_A,mu_a,mu_theta,
-                  sigma_w, sigma_A, sigma_a, sigma_theta)
+                           sigma_w, sigma_A, sigma_a, sigma_theta)
   
   x <- PVL_sims$x
   X <- PVL_sims$X
@@ -119,8 +133,9 @@ for (i in 1:niterations) {
   data <- list("x","X","ntrials","nsubs") 
   params<-c("mu_w","mu_A","mu_theta","mu_a","lambda_w","lambda_A","lambda_theta","lambda_a")
   samples <- jags.parallel(data, inits=NULL, params,
-                           model.file ="216377/Module4/hier_PVL.txt", n.chains=3, 
-                           n.iter=3000, n.burnin=1000, n.thin=1, n.cluster=4)
+                           model.file ="hier_PVL.txt",
+                           n.chains=3, n.iter=3000, n.burnin=1000, n.thin=1, n.cluster=64)
+  
   
   # mu
   true_mu_w[i] <- mu_w
@@ -148,20 +163,22 @@ for (i in 1:niterations) {
   infer_lambda_a[i] <- MPD(Y$lambda_a)
   
   print(i)
-  
 }
 
 end_time = Sys.time()
 end_time - start_time
 
+################ plots
+
 # let's look at some scatter plots
 # plotting code courtesy of Lasse
-source('216377/Module4/recov_plot.R')
+source('../../PVL/PVL_recovery/recov_plot.R')
 pl1 <- recov_plot(true_mu_w, infer_mu_w, c("true mu_w", "infer mu_w"), 'smoothed linear fit')
 pl2 <- recov_plot(true_mu_A, infer_mu_A, c("true mu_A", "infer mu_A"), 'smoothed linear fit')
 pl3 <- recov_plot(true_mu_theta, infer_mu_theta, c("true mu_theta", "infer mu_theta"), 'smoothed linear fit')
 pl4 <- recov_plot(true_mu_a, infer_mu_a, c("true mu_a", "infer mu_a"), 'smoothed linear fit')
 ggarrange(pl1, pl2, pl3, pl4)
+ggsave("jags_output_95t_247s_50i/hier_PVL_rec_95t_247s_50_i_mu") ##### CHANGE HERE
 
 # sigma (aka. true_lambda) re-coded as precision
 pl1 <- recov_plot(infer_lambda_w, 1/(true_lambda_w^2), c("infer lambda_w","true lambda_w"), 'smoothed linear fit')
@@ -169,6 +186,7 @@ pl2 <- recov_plot(infer_lambda_A, 1/(true_lambda_A^2), c("infer lambda_A","true 
 pl3 <- recov_plot(infer_lambda_theta, 1/(true_lambda_theta^2), c("infer lambda_theta", "true lambda_theta"), 'smoothed linear fit')
 pl4 <- recov_plot(infer_lambda_a, 1/(true_lambda_a^2), c("infer lambda_a", "true lambda_a"), 'smoothed linear fit')
 ggarrange(pl1, pl2, pl3, pl4)
+ggsave("jags_output_95t_247s_50i/hier_PVL_rec_95t_247s_50_i_lambda")  ##### CHANGE HERE
 
 # lambda (aka. infer_lambda) re-coded as SD
 pl1 <- recov_plot(1/sqrt(infer_lambda_w), true_lambda_w, c("infer lambda_w","true lambda_w"), 'smoothed linear fit')
@@ -176,36 +194,4 @@ pl2 <- recov_plot(1/sqrt(infer_lambda_A), true_lambda_A, c("infer lambda_A","tru
 pl3 <- recov_plot(1/sqrt(infer_lambda_theta), true_lambda_theta, c("infer lambda_theta", "true lambda_theta"), 'smoothed linear fit')
 pl4 <- recov_plot(1/sqrt(infer_lambda_a), true_lambda_a, c("infer lambda_a", "true lambda_a"), 'smoothed linear fit')
 ggarrange(pl1, pl2, pl3, pl4)
-
-
-###########################################################
-#---------- run the hierarchical model on controls --------
-###########################################################
-
-# x <- x_all
-# X <- X_all
-# 
-# ntrials <- ntrials_all
-# 
-# # set up jags and run jags model
-# data <- list("x","X","ntrials","nsubs") 
-# params<-c("mu_w","mu_A","mu_theta","mu_a","lambda_w","lambda_A","lambda_theta","lambda_a")
-# 
-# samples <- jags.parallel(data, inits=NULL, params,
-#                 model.file ="216377/Module4/hier_PVL.txt",
-#                 n.chains=3, n.iter=5000, n.burnin=1000, n.thin=1, n.cluster=4)
-# 
-# # let's look at the posteriors for the parameters
-# par(mfrow=c(2,2))
-# plot(density(samples$BUGSoutput$sims.list$mu_w))
-# plot(density(samples$BUGSoutput$sims.list$mu_A))
-# plot(density(samples$BUGSoutput$sims.list$mu_theta))
-# plot(density(samples$BUGSoutput$sims.list$mu_a))
-# 
-# # # let's look at the posteriors for the parameters
-# # par(mfrow=c(2,2))
-# # plot(density(samples$BUGSoutput$sims.list$lambda_w))
-# # plot(density(samples$BUGSoutput$sims.list$lambda_A))
-# # plot(density(samples$BUGSoutput$sims.list$lambda_theta))
-# # plot(density(samples$BUGSoutput$sims.list$lambda_a))
-
+ggsave("jags_output_95t_247s_50i/hier_PVL_rec_95t_247s_50_i_lambda_sd") 
