7488588d3bd8565305057a6b0298d9d051f07b1e
KathrineSN <Kathrinesn@live.dk>
2023-01-12 12:21:26 +0100

    cleaning

--- git_setup.sh
+++
@@ -1,6 +0,0 @@
-# run using
-# bash path_to_folder/setup_git.sh
-# !/bin/bash
-git config --global user.email "KathrineSN@live.dk"
-git config --global user.name "KathrineSN"
-git config --global user.password "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"
